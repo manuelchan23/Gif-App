db858c9a8d488315379b3175cea8689fa79bffc6
Manuel Chan <manuelchan23@gmail.com>
2021-04-23 06:31:15 -0500

    component AddCategory

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,11 +1,28 @@
 //rafce
-import React from 'react'
+import React,{ useState } from 'react'
 
 const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(["Goku","Gohan","krilin"]);
+
+  const addCategory = (e) => {
+    //setCategories([...categories, "Videl"]);
+    setCategories(cats => [...cats, "Bulma"]);
+  } 
+
   return (
     <>
+      
       <h2> GifExpertApp </h2>
       <hr />
+      <button onClick={ addCategory }>Agergar</button>
+      <ol>
+        {
+          categories.map( category => {
+            return <li key={ category }> { category } </li>
+          })
+        }
+      </ol>
     </>
   )
 }
